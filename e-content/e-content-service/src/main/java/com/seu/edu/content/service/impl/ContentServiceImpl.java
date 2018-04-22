0ee3a75bb7146fb19768837ce4d736928c393dc5
WINTERFELLS <734860108@qq.com>
2018-04-22 20:23:48 +0800

    user redis realized the cache of index.html content display

--- e-content/e-content-service/src/main/java/com/seu/edu/content/service/impl/ContentServiceImpl.java
+++ e-content/e-content-service/src/main/java/com/seu/edu/content/service/impl/ContentServiceImpl.java
@@ -3,10 +3,14 @@ package com.seu.edu.content.service.impl;
 import java.util.Date;
 import java.util.List;
 
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+import com.seu.edu.common.jedis.JedisClient;
 import com.seu.edu.common.utils.EResult;
+import com.seu.edu.common.utils.JsonUtils;
 import com.seu.edu.content.service.ContentService;
 import com.seu.edu.mapper.TbContentMapper;
 import com.seu.edu.pojo.TbContent;
@@ -19,21 +23,48 @@ public class ContentServiceImpl implements ContentService {
 	@Autowired
 	private TbContentMapper contentMapper;
 	
+	@Autowired
+	private JedisClient jedisClient;
+	
+	@Value("${CONTENT_LIST}")
+	private String CONTENT_LIST;
+	
 	@Override
 	public EResult addContent(TbContent tbContent) {
 		tbContent.setCreated(new Date());
 		tbContent.setUpdated(new Date());
 		
 		contentMapper.insert(tbContent);
+		jedisClient.hdel(CONTENT_LIST, tbContent.getCategoryId().toString());
 		return EResult.ok();
 	}
 
 	@Override
 	public List<TbContent> getContentListByCid(long cid) {
+		//查询缓存
+		try {
+			//如果缓存有直接响应
+			String json = jedisClient.hget(CONTENT_LIST, cid+"");
+			if(StringUtils.isNotBlank(json)) {
+				List<TbContent> list = JsonUtils.jsonToList(json, TbContent.class);
+				return list;
+			}
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		//如果没有查询数据库
 		TbContentExample example = new TbContentExample();
 		Criteria criteria = example.createCriteria();
 		criteria.andCategoryIdEqualTo(cid);
 		List<TbContent> list = contentMapper.selectByExampleWithBLOBs(example);
+		//结果添加到数据库
+		try {
+			jedisClient.hset("CONTENT_LIST", cid+"", JsonUtils.objectToJson(list));
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 		return list;
 	}
 
